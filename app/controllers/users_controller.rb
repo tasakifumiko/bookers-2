7fef23fce8812c5addaab8879e74531089704d11
TasakiFumiko <121810685+tasakifumiko@users.noreply.github.com>
2023-01-27 09:13:26 +0000

    レイアウトおおまかに完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,14 @@ class UsersController < ApplicationController
   
   def update
      @user = User.find(params[:id])
+     
+     
      if @user.update(user_params)
-      flash[:notice] =  "User info was successfully updated."
+      flash[:notice] =  "You habe updated user successfully."
       redirect_to user_path(@user.id)
      else
       render edit_user_path(@user.id)
      end
-     
-     
-     
-     
   end
   
   def edit
